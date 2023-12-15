f1b33556d3eb9fab9afc1adf2505ca084ce2f5e8
LindaVelozaM <135375596+LindaVelozaM@users.noreply.github.com>
2023-12-15 18:49:47 -0500

    fix

--- 06-DOM/app.js
+++ 06-DOM/app.js
@@ -37,7 +37,153 @@ contenido que tiene el texto*/
 // items[0].textContent = 'Prueba';
 
 /***************getElementsByTagName Devuelve el contenido de las etiquetas*/
-var items = document.getElementsByTagName('li');
-items[2].textContent = 'Prueba02';
+// var items = document.getElementsByTagName('li');
+// items[2].textContent = 'Prueba02';
 
+/*******************************
+ * querySelector = significa seleccionar el primer elemento 
+ */
+
+// var header = document.querySelector('#main-header');
+// header.style.borderBottom = 'solid 4px #ccc';
+
+// var input = document.querySelector('input');
+// input.value = 'Hola mundo';
+
+// var input1 = document.querySelector('input[type="submit"]');
+// input1.value = 'Ingresar';
+
+// var item = document.querySelector('.list-group-item');
+// item.style.color = 'red';
+
+/**************************
+ * querySelectorAll = significa seleccionar todos los elementos, funcionando como un arreglo, se
+ * debe indicar a cual de todos se le debe realizar el cambio ej: [2]
+ */
+
+// var items = document.querySelectorAll('.list-group-item');
+// items[2].style.color = 'red';
+
+// var titulos = document.querySelectorAll('.title');
+// console.log(titulos);
+// titulos[0].textContent = 'Prueba';
+
+//Para ponerle fondo a todos los items impares
+// var impar = document.querySelectorAll('li:nth-child(odd)');
+
+//Para ponerle fondo a todos los items pares
+// var par = document.querySelectorAll('li:nth-child(even)');
+
+// for(var i=0; i < impar.length; i++){
+//     impar[i].style.backgroundColor = '#ccc';
+//     par[i].style.backgroundColor = '#FF00FF';
+// }
+
+/*****************************
+ * ParentNode
+ */
+// var itemList = document.querySelector('#items');
+// console.log(itemList.parentNode);
+// var main = itemList.parentNode;
+// main.style.backgroundColor = '#f4f4f4';
+// console.log(main.parentNode.parentNode);
+
+/*****************************
+ * ParentElement
+ */
+
+// var itemList = document.querySelector('#items');
+// console.log(itemList.parentElement);
+// var main = itemList.parentElement;
+// main.style.backgroundColor = '#f4f4f4';
+// console.log(main.parentNode.parentElement);
+
+/*******************
+ * childNodes
+ */
+// console.log(itemList.childNodes);
+
+/*******************
+ * children = Niños
+ */
+// console.log(itemList.children);
+
+/*******************
+ * firstElementChild
+ */
+// console.log(itemList.firstElementChild);
+// itemList.firstElementChild.textContent = 'prueba';
+
+/*******************
+ * lastChild = 
+ */
+// console.log(itemList.lastElementChild);
+// itemList.lastElementChild.textContent = 'prueba';
+
+/******************
+ * previousSibling = Hermanos
+ */
+// console.log(itemList.previousSibling);
+
+/******************
+ * previousElementSibling
+ */
+// console.log(itemList.previousElementSibling);
+
+/******************
+ * nextSibling
+ */
+// console.log(itemList.nextSibling);
+
+/******************
+ * nextElementSibling
+ */
+// console.log(itemList.nextElementSibling);
+
+/***************************
+ * Crear elementos y nodos 
+ */
+/*******
+ * createElement
+ * tagName
+ */
+
+// var nuevoDiv = document.createElement('div');
+// nuevoDiv.className = 'hola';
+// nuevoDiv.id = 'div-hola';
+// nuevoDiv.setAttribute('title','Hola mundo');
+
+/*******
+ * createTextNode
+ */
+//poner texto dentro de la etiqueta creada anteriormente
+// var nuevoNodoText = document.createTextNode('Hola mundo');
+// nuevoDiv.appendChild(nuevoNodoText);
+
+// console.log(nuevoDiv);
+
+//ubicación donde queremos insertar el nuevo div
+// var contenedor = document.querySelector('.container');
+// console.log(contenedor);
+
+//posición donde queremos insertar nuestra etiqueta
+// var h1 = document.querySelector('h1');
+// console.log(h1);
+
+// contenedor.insertBefore(nuevoDiv, h1);
+
+/******************
+ * Agregar eventos = escuchas que estan dentro del sitio web a espera del usuario
+ */
+
+// document.getElementById('boton').addEventListener('click', hacerClick);
+
+// function hacerClick(){
+//     document.getElementById('header-title').textContent = 'Texto cambiado';
+//     // console.log('Usted hizo click')
+// }
+
+/****************
+ * Actualizar el diseño
+ */
 
